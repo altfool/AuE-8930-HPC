c826c116f90b79672ce3c68ca5cd0a184b5e92dd
Zack Yang <zongmiy@clemson.edu>
2020-10-26 20:06:14 -0400

    66632

--- Homework3/Q4/Q4A.py
+++ Homework3/Q4/Q4A.py
@@ -1,3 +1,6 @@
+import threading
+import time
+
 var = 0
 
 def add():
@@ -8,7 +11,26 @@ def minus():
     global var
     var = var - 1
 
-for i in range(5000000):
-    add()
-    minus()
-print(var)
+def main():
+    thread_list = []
+    start = time.time()
+    # for i in range(5000000):
+        # add()
+        # minus()
+    for i in range(5000000):
+        thd = threading.Thread(target=add, args=None)
+        thread_list.append(thd)
+        thd = threading.Thread(target=minus, args=None)
+        thread_list.append(thd)
+    
+    for thread in thread_list:
+        thread.start()
+    for thread in thread_list:
+        thread.join()
+
+    end = time.time()
+    print("Running Time: %.5f s" % (end - start))
+    print(var)
+
+if __name__ == '__main__':
+    main()
